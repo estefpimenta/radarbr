b0ee3c5dc223249fb532397c68dbfbd0fc1982d7
estefanno pimenta <estefanno.pimenta@gmail.com>
2026-07-02 10:26:12 -0300

    add css: reset, global and variables (design system)

---
+++ src/components/App/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1>RadarBR</h1> 
+     <h2>Bem-vindo ao RadarBR, o seu portal de informações sobre o Brasil!</h2>
+     <p>O RadarBR é o seu portal de informações sobre o Brasil!</p>
+    </>
+  )
+}
+
+export default App

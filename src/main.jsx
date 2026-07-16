44ae53f3cb54147304811fb82d107fc3048bb1ed
estefanno pimenta <estefanno.pimenta@gmail.com>
2026-07-16 15:52:26 -0300

    on mains.jsx: fix: configura basename para GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/reset.css'
-import './styles/variables.css'
-import './styles/global.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/reset.css";
+import "./styles/variables.css";
+import "./styles/global.css";
 
-import App from './components/App/App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import App from "./components/App/App.jsx";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-
-    <BrowserRouter>
+    <BrowserRouter basename="/radarbr">
       <App />
     </BrowserRouter>
-    
   </StrictMode>,
-)
+);

401dac4dc64465b319e5269f1ae6878175b05a9a
estefanno pimenta <estefanno.pimenta@gmail.com>
2026-07-21 15:44:07 -0300

    update logo color

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,18 +1,23 @@
-import './NavBar.css'
-import radarIcon from '../../assets/radarIcon.png'
+import "./NavBar.css";
+import radarIcon from "../../assets/radarIcon.png";
 
 function NavBar() {
-
-    return(
-        <div className="navBar">
-            <div className="navBar__logo">
-                <div className="navBar__logo-container">
-                    <img className='navBar__logo-img' src={radarIcon} alt="RadarBR icon" />
-                </div>
-                <h1 className="navBar__logo-text">RadarBR</h1>
-            </div>
+  return (
+    <div className="navBar">
+      <div className="navBar__logo">
+        <div className="navBar__logo-container">
+          <img
+            className="navBar__logo-img"
+            src={radarIcon}
+            alt="RadarBR icon"
+          />
         </div>
-    )
+        <h1 className="navBar__logo-text">
+          <span className="navBar__logo-text-Radar">Radar</span>BR
+        </h1>
+      </div>
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
